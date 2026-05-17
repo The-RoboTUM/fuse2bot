e6dce8f0ac65c2f9616881cab63477b30b37f196
Linus Schwarz <public.linus.schwarz@gmail.com>
2026-05-17 12:36:04 +0200

    Added better formatting

--- fuse2bot/core/utils.py
+++ fuse2bot/core/utils.py
@@ -1,5 +1,5 @@
 def format_urdf_name(name: str) -> str:
-    ''' Format a string to be a valid URDF name
+    """Format a string to be a valid URDF name
 
     Parameters
     ----------
@@ -10,5 +10,8 @@ def format_urdf_name(name: str) -> str:
     -------
     str
         formatted name
-    '''
-    return name.replace(':','_').replace(' ','').replace('-','_').lower()
+    """
+    name = name.replace(":", "_").replace(" ", "").replace("-", "_").lower()
+    # also replace ending with "v28_1" with "_1"
+    name = re.sub(r"v\d+(_\d+$)", r"\1", name)
+    return name
